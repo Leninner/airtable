d7eee99b8c0285cd4b5708ce68bb918c70813f62
leninner <mazabandalenin180@gmail.com>
2021-10-12 20:18:25 -0500

    Añado mejoras

--- automatizacionAirtable.js
+++ automatizacionAirtable.js
@@ -1,5 +1,6 @@
 const URL = window.location.href;
 
+// Keywords por área
 const tecnología = [
   'developer',
   'front-end',
@@ -14,12 +15,17 @@ const tecnología = [
   'programador',
   'qa',
   'software',
+  'ios',
+  'android',
 ];
+const ventas = ['ventas', 'comercial', 'vendedor', 'call'];
+const marketing = ['marketing', 'brand', 'planner', 'community'];
+const innovacion = ['scrum', 'CTO'];
+const logistica = ['logistica', 'logística'];
+const finanzas = ['financiero', 'credito', 'crédito', 'banca', 'tesorería', 'tesoreria'];
+const ejecutivo = ['gerente', 'ejecutivo', 'ejecutiva', 'jefe', 'jefa'];
 
-const ventas = ['ventas', 'comercial'];
-
-const marketing = ['marketing', 'brand', 'planner'];
-
+// Funciones para asignar el área en cada plataforma de trabajo
 function isAreaLinkedIn(job) {
   if (job.outerText.split(' ').some((word) => tecnología.includes(word.toLowerCase()) == true)) {
     return 'Tecnología';
@@ -27,15 +33,21 @@ function isAreaLinkedIn(job) {
     return 'Ventas';
   } else if (job.outerText.split(' ').some((word) => marketing.includes(word.toLowerCase()) == true)) {
     return 'Marketing';
+  } else if (job.outerText.split(' ').some((word) => innovacion.includes(word.toLowerCase()) == true)) {
+    return 'Innovación';
+  } else if (job.outerText.split(' ').some((word) => logistica.includes(word.toLowerCase()) == true)) {
+    return 'Logística';
+  } else if (job.outerText.split(' ').some((word) => finanzas.includes(word.toLowerCase()) == true)) {
+    return 'Finanzas';
+  } else if (job.outerText.split(' ').some((word) => ejecutivo.includes(word.toLowerCase()) == true)) {
+    return 'Dirección';
   }
 }
-
 function isAreaMultitrabajos(job) {}
-
 function isAreaJooble(job) {}
-
 function isAreaComputrabajo(job) {}
 
+// Funciones para buscar empleos en los distintos portales de empleos
 function linkedInFindJobs() {
   const jobs = document.querySelectorAll('.job-card-list__title');
   const empresas = document.querySelectorAll('.job-card-container__company-name');
@@ -58,7 +70,9 @@ function linkedInFindJobs() {
             ? 'Remoto'
             : ciudades[i].outerText.split(',')[0].toString(),
         link: jobs[i].href,
-        nivel: jobs[i].outerText.includes('Ejecutivo') ? 'Ejecutivo' : 'Especialista',
+        nivel: jobs[i].outerText.split(' ').some((word) => ejecutivo.includes(word.toLowerCase()) == true)
+          ? 'Ejecutivo'
+          : 'Especialista',
         area: isAreaLinkedIn(jobs[i]),
       });
     }
@@ -71,7 +85,6 @@ function linkedInFindJobs() {
 
 function multitrabajosFindJobs() {
   const datos = document.querySelectorAll('.Card__CardContentWrapper-sc-i6v2cb-1');
-
   function findJobs(datos) {
     let jobsMultitrabajos = [];
     datos.forEach((value) => {
@@ -95,17 +108,21 @@ function joobleFindJobs() {
   let jobsJooble = [];
   const empleos = document.querySelectorAll('._2caa5._5d7c4');
 
+  function existCities(existCity) {
+    return existCity.length > 2
+      ? existCity[1].children[1].childNodes[0].textContent.split(',')[0].toString()
+      : existCity[0].children[0].childNodes[0].textContent.split(',')[0].toString();
+  }
+
   function findJobs(empleos) {
     empleos.forEach((value) => {
       if (value.children[1].children[1].children.length > 1) {
         let existCity = value.children[1].children[1].children[1].children;
+
         jobsJooble.push({
           vacante: value.children[0].outerText,
           empresa: existCity.length > 2 ? existCity[0].outerText : 'Jooble',
-          ciudad:
-            existCity.length > 2
-              ? existCity[1].children[1].childNodes[0].textContent.split(',')[0].toString()
-              : existCity[0].children[0].childNodes[0].textContent.split(',')[0].toString(),
+          ciudad: existCities(existCity),
           link: value.children[0].children[0].children[0].href,
         });
       } else {
@@ -152,6 +169,7 @@ function computrabajosFindJobs() {
   copy(findJobs(datos));
 }
 
+// Función padre para buscar empleos
 function findJobs(URL) {
   switch (true) {
     case URL.includes('linkedin'):
